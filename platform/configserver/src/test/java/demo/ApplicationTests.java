0b885f371e20df1b25cb47b924c8816f6e688121
Brad Futch <bradfutch@gmail.com>
2019-04-08 10:55:21 -0500

    attempts to deal with dependency hell

--- platform/configserver/src/test/java/demo/ApplicationTests.java
+++ platform/configserver/src/test/java/demo/ApplicationTests.java
@@ -7,20 +7,20 @@ import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ConfigServerApplication.class)
+@SpringBootTest(classes = ConfigServerApplication.class)
 @WebAppConfiguration
-@IntegrationTest("server.port=0")
+@TestPropertySource(properties="server.port=0")
 public class ApplicationTests {
 
 	@Value("${local.server.port}")
