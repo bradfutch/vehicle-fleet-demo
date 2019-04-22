bb9f9d5677ad1f91cb5bc07c1deb29b82dce02a8
Brad Futch <bradfutch@gmail.com>
2019-04-22 12:26:19 -0700

    attempting to add some channel config for the outbound channel?

--- fleet-location-updater/src/main/java/demo/WebSocketConfig.java
+++ fleet-location-updater/src/main/java/demo/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -16,6 +17,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		config.enableSimpleBroker("/topic/vehicles");;
 		config.setApplicationDestinationPrefixes("/app");
 	}
+
+	@Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(4).maxPoolSize(10);
+    }
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
